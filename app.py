bd7e9650d0a735e30a716d91a52425d413d323a5
DrJonnyMoney <jonny_huang@ite.edu.sg>
2025-03-12 18:10:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,12 @@ from flask import Flask, send_from_directory, render_template, request
 import os
 import sys
 base_prefix = os.environ.get('NB_PREFIX', '')
-
-print(base_prefix)
+print('URL@' + base_prefix)
 # Create Flask app
 app = Flask(__name__)
 
-@app.route('/<path:path>')
-def catch_all(path):
-    print(f"Caught path: {path}", file=sys.stderr)
-    print(f"Full URL: {request.url}", file=sys.stderr)
-    print(f"Method: {request.method}", file=sys.stderr)
-    print(f"Headers: {dict(request.headers)}", file=sys.stderr)
+@app.route(base_prefix + '/')
+def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
